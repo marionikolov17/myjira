26fecb7360a659c5166ad1b47c9fb7701edba6a2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2026-03-09 22:54:25 +0200

    Setup: typescript jest and istanbul

---
+++ server/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
