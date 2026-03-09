b49ed4994762880e468066784f534c9782ed1ef3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2026-03-09 21:37:38 +0200

    chore: replace console logs with new logger

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import { env } from './config/env';
+import { logger } from './common/logger';
 
 app.listen(env.PORT, () => {
-  console.log(`Server is listening on PORT ${env.PORT}`);
+  logger.info(`Server is listening on PORT ${env.PORT}`, { port: env.PORT });
 });
