bb8821999a0e958334d41aa5e76f785fd195d4dd
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2026-03-15 12:36:10 +0200

    feat: abstract password hasher

---
+++ server/src/common/password-hasher/index.ts
@@ -0,0 +1,4 @@
+export * from './password-hasher.interface';
+export * from './create-password-hasher';
+export * from './password-hasher';
+export * from './bcrypt-password-hasher';
--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -4,6 +4,7 @@ import { NodeEnv } from '@/config/env.types';
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(Object.values(NodeEnv)).default(NodeEnv.DEVELOPMENT),
+  SALT_ROUNDS: z.coerce.number().min(4).max(31).default(10),
 });
 
 export const env = envSchema.parse(process.env);
