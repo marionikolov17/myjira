0a85a04fe66fcb114bcae7612cc0fe3935ad60ed
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2026-04-25 16:29:50 +0300

    feat: add debug logging for prisma queries

--- server/src/common/lib/prisma.ts
+++ server/src/common/lib/prisma.ts
@@ -1,6 +1,15 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../../prisma/generated/prisma/client';
+import { logger } from '../logger';
 import { env } from '@/config/env';
 
 const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
-export const prismaClient = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter, log: ['query', 'info', 'warn', 'error'] });
+
+prisma.$on('query', (e) => {
+  logger.debug('Query: ' + e.query);
+  logger.debug('Params: ' + e.params);
+  logger.debug('Duration: ' + e.duration + 'ms');
+});
+
+export { prisma };
